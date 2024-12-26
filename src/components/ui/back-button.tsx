1fbca92b538891d52c3ac3a58ac40e1970647c8b
Mrigank Doshy <mrigankdoshy@gmail.com>
2024-12-26 14:52:35 -0500

    refactor: pull out back button into component

---
+++ src/components/ui/back-button.tsx
@@ -0,0 +1,21 @@
+import { ChevronLeft } from 'lucide-react';
+import Link from 'next/link';
+
+import { cn } from '@/lib/utils';
+
+import { buttonVariants } from '@/components/ui/button';
+
+export function BackButton() {
+  return (
+    <Link
+      href="/"
+      className={cn(
+        buttonVariants({ variant: 'ghost' }),
+        'absolute left-4 top-4 md:left-8 md:top-8'
+      )}
+    >
+      <ChevronLeft className="mr-2 size-4" />
+      Back
+    </Link>
+  );
+}
